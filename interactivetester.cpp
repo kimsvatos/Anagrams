f5ca0f187663930428a9262b599d39ca77b2308e
kimsvatos <kimsvatos@ucla.edu>
2015-10-23 12:44:19 -0700

    Create interactivetester.cpp

---
+++ interactivetester.cpp
@@ -0,0 +1,69 @@
+
+#if defined(_MSC_VER)
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+struct KeepWindowOpenUntilDismissed
+{
+    ~KeepWindowOpenUntilDismissed()
+    {
+        DWORD pids[1];
+        if (GetConsoleProcessList(pids, 1) == 1)
+        {
+            std::cout << "Press any key to continue . . . ";
+            _getch();
+        }
+    }
+} keepWindowOpenUntilDismissed;
+#endif
+
+
+#include "Dictionary.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void anagramCallback(string anagram);
+
+int main()
+{
+    cout << "Enter name of dictionary file: ";
+    string filename;
+    getline(cin, filename);
+    ifstream wordFile(filename.c_str());
+    if ( ! wordFile)
+    {
+        cout << "Cannot open " << filename << endl;
+        return 1;
+    }
+
+    Dictionary dict;
+    
+    string word;
+    while (getline(wordFile, word))
+        dict.insert(word);
+    wordFile.close();
+
+    for (;;)
+    {
+        cout << "Enter a set of letters: ";
+        string letters;
+        getline(cin,letters);
+        if ( ! cin  ||  letters.empty())
+            break;
+
+        cout << "Anagrams of " << letters << ":\n";
+
+        dict.lookup(letters, anagramCallback);
+        
+        cout << endl;
+    }
+}
+
+void anagramCallback(string anagram)
+{
+    cout << "  " << anagram;
+}
